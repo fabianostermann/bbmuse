381715edb846acb9ae0ac8f41e911ebf85fdb4bf
Fabian Ostermann <fabian.ostermann@tu-dortmund.de>
2025-11-14 01:24:25 +0100

    Add a few group identifiers to DummyProject (as a unit test)

--- examples/DummyProject/modules/core/ModA.py
+++ examples/DummyProject/modules/core/ModA.py
@@ -1,9 +1,13 @@
+GROUP = "special"
 USES = [ "Clock" ]
 PROVIDES = [ "RepA" ]
+
+import time
 
 internal_var = 0
 
 def _update(bb):
+    time.sleep(0.2)
     #bb.RepA = "Overwriting blackboard entries should be forbidden."
 
     bb.RepA.ValueA += 2
--- examples/DummyProject/modules/core/ModOut.py
+++ examples/DummyProject/modules/core/ModOut.py
@@ -1,7 +1,10 @@
+GROUP = "output-group"
 REQUIRES = [ "BibRep" ]
 PROVIDES = [ "RepOut1", "RepOut2", ]    
 
-def _update(bb):
-    print("I am providing outside of the main loop without requirements!")
+from time import sleep
 
+def _update(bb):
+    print("I am providing from a special group thread.")
+    sleep(0.1)
     #print("I should not be allowed to do this: clock.now =", bb["Clock"].now)
