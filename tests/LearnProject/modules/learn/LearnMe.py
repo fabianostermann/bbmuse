8b3e48add0884feaaba6fb85877c71eaff86c55d
Fabian Ostermann <fabian.ostermann@tu-dortmund.de>
2026-04-16 23:59:24 +0200

    Update LearnProject to output random numbers for dummy episodes

--- tests/LearnProject/modules/learn/LearnMe.py
+++ tests/LearnProject/modules/learn/LearnMe.py
@@ -3,7 +3,15 @@ REQUIRES = [ "ReqRep" ]
 PROVIDES = [ "ProvRep", "UsedRep" ]
 
 import time
+import random
 
 def _update(bb):
-    print("Can you learn this???")
-    time.sleep(3)
+    print("Updating everything..")
+
+    bb.ProvRep.valueA = random.uniform(-2, 2)
+    bb.ProvRep.valueB = random.randint(-10, 10)
+
+    bb.UsedRep.valueA = random.uniform(-2, 2)
+    bb.UsedRep.valueB = random.randint(-10, 10)
+
+    time.sleep(0.1)
